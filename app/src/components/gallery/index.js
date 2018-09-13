6d3e9bc9cf60b9142f65093b2dab8b165b03df18
Liam Ellis <liamhellis@gmail.com>
2018-09-13 18:04:02 -0400

    cant even remember

--- app/src/components/gallery/index.js
+++ app/src/components/gallery/index.js
@@ -1,117 +1,117 @@
-import React, { Component } from 'react'
-import { processPortfolioLines, cornerLines } from '../lines'
-import Lightbox from 'react-images'
-import {Thumb} from '../thumb'
-import ScrollReveal from 'scrollreveal'
-import AppStore from '../../flux/stores'
-import LightBoxTheme from '../lightboxTheme'
-import './_gallery.scss'
+import React, { Component } from "react";
+import { processPortfolioLines, cornerLines } from "../lines";
+import Lightbox from "react-images";
+import { Thumb } from "../thumb";
+import ScrollReveal from "scrollreveal";
+import AppStore from "../../flux/stores";
+import LightBoxTheme from "../lightboxTheme";
+import "./_gallery.scss";
 
 export default class Gallery extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       currentImage: 0,
       lightboxIsOpen: false,
       photos: []
-    }
+    };
   }
 
-  componentDidMount () {
-    processPortfolioLines()
-    cornerLines()
+  componentDidMount() {
+    processPortfolioLines();
+    cornerLines();
 
-    const { galleryItems } = AppStore.data
-    const gridContainer = document.querySelector('.thumb__scroller')
-    const scroller = ScrollReveal()
+    const { galleryItems } = AppStore.data;
+    const gridContainer = document.querySelector(".thumb__scroller");
+    const scroller = ScrollReveal();
 
-    scroller.reveal('.grid__item__thumb', {
-      origin: 'left',
+    scroller.reveal(".grid__item__thumb", {
+      origin: "left",
       container: gridContainer,
-      distance: '40px',
+      distance: "40px",
       duration: 1000,
       delay: 100
-    })
+    });
 
     const photoSet = galleryItems.map((item, i) => {
-      let gridItem = {...item}
-      gridItem.src = item.url
-      gridItem.caption = `${item.property}, ${item.address}`
-      return gridItem
-    })
+      let gridItem = { ...item };
+      gridItem.src = item.url;
+      gridItem.caption = `${item.property}, ${item.address}`;
+      return gridItem;
+    });
 
     this.setState({
       photos: photoSet
-    })
+    });
   }
-  
+
   closeLightbox = () => {
     this.setState({
       currentImage: 0,
       lightboxIsOpen: false
-    })
-  }
+    });
+  };
 
   openLightbox = (i, e) => {
-    e.preventDefault()
-    console.log(i)
+    e.preventDefault();
     this.setState({
       currentImage: i,
       lightboxIsOpen: true
-    })
-  }
+    });
+  };
 
   goToNext = () => {
     this.setState({
       currentImage: this.state.currentImage + 1
-    })
-  }
+    });
+  };
 
   goToPrevious = () => {
     this.setState({
       currentImage: this.state.currentImage - 1
-    })
-  }
+    });
+  };
 
-    goToImage = index => {
+  goToImage = index => {
     this.setState({
       currentImage: index
-    })
-  }
+    });
+  };
   handleClickImage = () => {
-    if (this.state.currentImage === this.props.images.length - 1) return
-    this.goToNext()
-  }
+    if (this.state.currentImage === this.props.images.length - 1) return;
+    this.goToNext();
+  };
 
-
-  render () {
-    const { galleryItems } = AppStore.data
-    const {photos} = this.state
+  render() {
+    const { galleryItems } = AppStore.data;
+    const { photos } = this.state;
     return (
-      <main className='gallery loading component gallery_component'>
-        <h1 className='page_title'>
-          Gallery
-        </h1>
-        <section className='wrapper'>
-          <div className='thumb__scroller'>
+      <main className="gallery loading component gallery_component">
+        <h1 className="page_title">Gallery</h1>
+        <section className="wrapper">
+          <div className="thumb__scroller">
             {galleryItems.map((item, i) => (
-              <Thumb item={item} key={i} openLightbox={this.openLightbox.bind(this, i)} />
+              <Thumb
+                item={item}
+                key={i}
+                openLightbox={this.openLightbox.bind(this, i)}
+              />
             ))}
           </div>
         </section>
         <Lightbox
-              images={photos}
-              backdropClosesModal={true}
-              onClose={this.closeLightbox}
-              onClickPrev={this.goToPrevious}
-              onClickNext={this.goToNext}
-              onClickImage={this.handleClickImage}
-              onClickThumbnail={this.goToImage}
-              currentImage={this.state.currentImage}
-              isOpen={this.state.lightboxIsOpen}
-              theme={LightBoxTheme}
-            />
+          images={photos}
+          backdropClosesModal={true}
+          onClose={this.closeLightbox}
+          onClickPrev={this.goToPrevious}
+          onClickNext={this.goToNext}
+          onClickImage={this.handleClickImage}
+          onClickThumbnail={this.goToImage}
+          currentImage={this.state.currentImage}
+          isOpen={this.state.lightboxIsOpen}
+          theme={LightBoxTheme}
+        />
       </main>
-    )
+    );
   }
 }
