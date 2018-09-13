80d45baac88e4454e93647fd18d72cbdb89c5ac1
Liam Ellis <liamhellis@gmail.com>
2018-09-13 16:35:38 -0400

    bug on portfoliocard formatting

--- app/src/components/portfolioCard/index.js
+++ app/src/components/portfolioCard/index.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import { HorizontalPortfolioCard } from './horizontalCard'
-import { VerticalPortfolioCard } from './verticalCard'
-import { BROWSER } from '../../utils/browser'
-import './_portfolioCard.scss'
-
-export const PortfolioCard = ({property, arrowClick, index, property: {featuredImage}}) => {
-  if (BROWSER.isMobile() || (featuredImage.fields.file.details.image.width >= featuredImage.fields.file.details.image.height)) {
-    return <HorizontalPortfolioCard index={index} arrowClick={arrowClick} property={property} />
+import React from "react";
+import { HorizontalPortfolioCard } from "./horizontalCard";
+import { VerticalPortfolioCard } from "./verticalCard";
+import { BROWSER } from "../../utils/browser";
+import "./_portfolioCard.scss";
+const { status } = BROWSER.isMobile();
+console.log(status);
+export const PortfolioCard = ({
+  property,
+  arrowClick,
+  index,
+  property: { featuredImage }
+}) => {
+  if (
+    status ||
+    featuredImage.fields.file.details.image.width >=
+      featuredImage.fields.file.details.image.height
+  ) {
+    return (
+      <HorizontalPortfolioCard
+        index={index}
+        arrowClick={arrowClick}
+        property={property}
+      />
+    );
   } else {
-    return <VerticalPortfolioCard index={index} arrowClick={arrowClick} property={property} />
+    return (
+      <VerticalPortfolioCard
+        index={index}
+        arrowClick={arrowClick}
+        property={property}
+      />
+    );
   }
-}
+};
