d9c3c976073c88ecdbd3b1bbf1c9b3fe9151031f
Liam Ellis <liamhellis@gmail.com>
2018-09-17 15:54:25 -0400

    css mobile

--- app/stories/dummy-data.js
+++ app/stories/dummy-data.js
@@ -33,3 +33,10 @@ export const propertyTwo = {
   description:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 };
+
+export const aboutUs = {
+  content:
+    "Highpoint Property Group is a New York City real estate developer, operator, owner, and manager with a primary focus on residential and mixed-use assets throughout the Tri-State area.↵↵We launched our platform in January 2017. Since then, Highpoint either owns, has developed and is operating, or is in contract for 15 properties representing over $250 million of value. We have joint venture partnerships with institutional investors as well as high net worth individuals and family office partners. In addition, we have well-established relationships with many major lending institutions.↵↵We are a firm that has a unique vision and expertise in every aspect of the development process. The Executive Team at Highpoint collectively has nearly 40 years’ worth of New York City-specific real estate experience: sourcing, identifying, acquiring, investing, managing, designing, developing, building, and handling the day-to-day operations of over $600 million of real estate acquisitions, $1 billion of debt and equity transactions, and well in excess of 3 million square feet.↵↵Young, aggressive, experienced and disciplined, the Highpoint Team has an unparalleled drive and determination. These qualities translate into a proven track record and superior risk-adjusted returns.",
+  tag: "about-us",
+  title: "About Us"
+};
--- app/stories/portfolio.stories.js
+++ app/stories/portfolio.stories.js
@@ -4,8 +4,10 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 import { PortfolioCard } from "../src/components/portfolioCard";
+import Portfolio from "../src/components/portfolio";
 
-import { propertyOne, propertyTwo } from "./dummy-data";
+import { propertyOne, propertyTwo, aboutUs } from "./dummy-data";
+
 import "../src/sass/_base.scss";
 import "../src/sass/_typography.scss";
 import "../src/sass/_variables.scss";
@@ -14,10 +16,14 @@ import "../src/App.scss";
 import "../src/components/portfolioCard/_portfolioCard.scss";
 import "../src/components/viewProcessButton/_index.scss";
 
+storiesOf("Portfolio/Page", module).add("View", () => (
+  <Portfolio properties={[propertyOne, propertyTwo]} aboutUsContent={aboutUs} />
+));
+
 storiesOf("Portfolio/Cards/Horizontal", module).add("Card", () => (
   <PortfolioCard mobileStatus={false} property={propertyOne} />
 ));
 
 storiesOf("Portfolio/Cards/Vertical", module).add("Card", () => (
-  <PortfolioCard mobileStatus={false} property={propertyTwo} />
+  <PortfolioCard mobileStatus={false} property={propertyTwo} about />
 ));
